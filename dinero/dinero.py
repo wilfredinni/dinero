6d20f7eac1c8bbcb53a40b02e239082b585f013b
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-10-18 20:28:51 -0300

    chore: repr and str

--- dinero/dinero.py
+++ dinero/dinero.py
@@ -1,10 +1,11 @@
+from dataclasses import dataclass
 from ._types import Currency
 
 
+@dataclass
 class Dinero:
-    def __init__(self, amount: int | float, currency: Currency):
-        self.amount = amount
-        self.currency = currency
+    amount: int | float
+    currency: Currency
 
     @property
     def raw_amount(self):
@@ -27,8 +28,16 @@ class Dinero:
         return self.currency.get("base")
 
     def formatted_amount(self, symbol: bool = False, currency: bool = False) -> str:
-        currency_format = f",.{self.currency.get('exponent')}f"
+        currency_format = f",.{self.exponent}f"
 
         currency_symbol = self.symbol if symbol else ""
         currency_code = f" {self.code}" if currency else ""
         return f"{currency_symbol}{self.amount:{currency_format}}{currency_code}"
+
+    def __repr__(self):
+        formatted_output = self.formatted_amount(symbol=True, currency=True)
+        return f"Dinero({self.raw_amount} -> {formatted_output})"
+
+    def __str__(self):
+        formatted_output = self.formatted_amount(symbol=True, currency=True)
+        return f"{self.raw_amount} -> {formatted_output}"
