c54e3396d68c109ffd18fabd810d83a1863aa51d
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-10-22 16:39:50 -0300

    feat: normalized amount

--- dinero/dinero.py
+++ dinero/dinero.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from decimal import Decimal, getcontext
+
 from ._types import Currency
 
 
@@ -27,12 +29,19 @@ class Dinero:
     def base(self):
         return self.currency.get("base")
 
+    @property
+    def normalized_amount(self):
+        getcontext().prec = self.base
+        print(type(Decimal(self.amount).normalize()))
+        return Decimal(self.amount).normalize()
+
     def formatted_amount(self, symbol: bool = False, currency: bool = False) -> str:
         currency_format = f",.{self.exponent}f"
+        normalized_amount = self.normalized_amount
 
         currency_symbol = self.symbol if symbol else ""
         currency_code = f" {self.code}" if currency else ""
-        return f"{currency_symbol}{self.amount:{currency_format}}{currency_code}"
+        return f"{currency_symbol}{normalized_amount:{currency_format}}{currency_code}"
 
     def __repr__(self):
         formatted_output = self.formatted_amount(symbol=True, currency=True)
