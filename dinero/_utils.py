871dcdea5d36859839af16bf2a7f99ed3edab160
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-11-05 12:51:58 -0300

    minor fix

--- dinero/_utils.py
+++ dinero/_utils.py
@@ -3,12 +3,12 @@ from decimal import Decimal
 
 
 class DecimalEncoder(json.JSONEncoder):
-    def default(self, object):
+    def default(self, obj):
         # if passed in object is instance of Decimal
         # convert it to a string, otherwise use the,
         # default behavior
         return (
-            str(object)
-            if isinstance(object, Decimal)
-            else json.JSONEncoder.default(self, object)
+            str(obj)
+            if isinstance(obj, Decimal)
+            else json.JSONEncoder.default(self, obj)
         )
